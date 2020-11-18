fad5f277721dec6d55281bc0d5329e324c665a12
Naveena Attota <naveena.attota@gmail.com>
2020-11-17 23:34:37 -0800

    Staywell external API complete

--- backend/app.py
+++ backend/app.py
@@ -1,46 +1,22 @@
 from flask import Flask, request
 import requests
+from staywell_api import StaywellExternalAPI
 
 app = Flask(__name__)
-staywellAPI = ("http://poc.select.kramesstaywell.com/Content/calculators-v1/"
-               "calorie-burn-rate-calculator")
 
 @app.route('/')
 def index():
     return 'Better You'
 
+# example: http://localhost:5000/staywell?weight=130&workout=Bowling&minutes=30&user=1
 @app.route('/staywell')
 def staywell():
     args = request.args
     if not args:
         return "Arguments needed.", 400
-    weight_param = check_weight(args)
-    if weight_param['status_code'] != 200:
-        return weight_param['msg'], weight_param['status_code']
-    else:
-        weight = weight_param['weight']
-    params = {'CalorieBurnCalc_Parameters': weight}
-    # this API is stupid so I need to write a class to basically reimplement their code :(
-    r  = requests.get(url=staywellAPI, params=params)
-    if not r:
-        return "Staywell API error", 500
-    if int(r.status_code) != 200:
-        return "Staywell API error", 500
-    print(r.text)
-    return "tbd"
+    api = StaywellExternalAPI()
+    return api.staywell(args)
 
-def check_weight(data):
-    if 'weight' in data:
-        weight = data['weight']
-        try:
-            weight = int(weight)
-            return {"status_code": 200, "weight": weight}
-        except ValueError:
-            return {"msg": "Please enter an integer for weight",
-                    "status_code": 400}
-    else:
-        return {"msg": "Please enter a weight",
-                "status_code": 400}
 
 if __name__ == '__main__':
 	app.run(host="localhost", port=5000, debug=False)
---
+++ backend/staywell_api.py
@@ -0,0 +1,220 @@
+from db import DB
+
+# source: http://poc.select.kramesstaywell.com/Content/calculators-v1/calorie-burn-rate-calculator
+# code/values/workout types come from this website
+
+class StaywellExternalAPI:
+    def __init__(self):
+        self.workout_list = ['Aerobics, step: high impact',
+                             'Aerobics, step: low impact',
+                             'Aerobics: high impact',
+                             'Aerobics: low impact',
+                             'Aerobics: water',
+                             'Basketball: game',
+                             'Basketball: wheelchair',
+                             'Bicycling, stationary: moderate',
+                             'Bicycling, stationary: vigorous',
+                             'Bicycling: 20 mph',
+                             'Bicycling: 12-13.9 mph',
+                             'Bicycling: 14-15.9 mph',
+                             'Bicycling: 16-19 mph',
+                             'Bicycling: BMX/mountain',
+                             'Bowling',
+                             'Boxing: sparring',
+                             'Calisthenics: moderate',
+                             'Calisthenics: vigorous',
+                             'Chopping/splitting wood',
+                             'Circuit training',
+                             'Dancing: disco, ballroom, square',
+                             'Dancing: fast, ballet, twist',
+                             'Dancing: slow, waltz, foxtrot',
+                             'Elliptical',
+                             'Football: competitive',
+                             'Football: touch, flag',
+                             'Gardening',
+                             'Golf: carrying clubs',
+                             'Golf: using cart',
+                             'Gymnastics',
+                             'Handball',
+                             'Health rider',
+                             'Heavy cleaning: car, windows',
+                             'Hiking: cross country',
+                             'Hockey: field and ice',
+                             'Ice skating',
+                             'Kayaking',
+                             'Martial arts: karate, kickboxing',
+                             'Moving: carrying boxes',
+                             'Mowing lawn: push, hand',
+                             'Mowing lawn: push, power',
+                             'Operate snow blower: walking',
+                             'Racquetball: casual',
+                             'Racquetball: competitive',
+                             'Raking lawn',
+                             'Rollerblade: skating',
+                             'Rope jumping',
+                             'Rowing, stationary: moderate',
+                             'Rowing, stationary: vigorous',
+                             'Running: 6 min/mile',
+                             'Running: 8 min/mile',
+                             'Running: 10 min/mile',
+                             'Running: 12 min/mile',
+                             'Scuba or skin diving',
+                             'Shoveling snow: by hand',
+                             'Sitting: reading, watching TV',
+                             'Skateboarding',
+                             'Skiing: downhill',
+                             'Ski machine',
+                             'Sleeping',
+                             'Snow shoeing',
+                             'Soccer',
+                             'Softball',
+                             'Stair-step machine',
+                             'Stretching, yoga',
+                             'Swimming: general',
+                             'Swimming: laps, vigorous',
+                             'Tai chi',
+                             'Tennis',
+                             'Walk/Jog: jog<10 min.',
+                             'Walk: 15 min/mile',
+                             'Water polo',
+                             'Water skiing',
+                             'Water volleyball',
+                             'Weightlifting: general',
+                             'Weightlifting: vigorous',
+                             'Whitewater: rafting, kayaking',
+                             'Wrestling',
+                             'Volleyball: beach',
+                             'Volleyball general play']
+
+    def staywell(self, args):
+        if not args:
+            return "Arguments needed.", 400
+        weight_param = self.check_weight(args)
+        if weight_param['status_code'] != 200:
+            return weight_param['msg'], weight_param['status_code']
+        else:
+            weight = weight_param['weight']
+        workout_param = self.check_workout_type(args)
+        if workout_param['status_code'] != 200:
+            return workout_param['msg'], workout_param['status_code']
+        else:
+            workout = workout_param['workout']
+        mins_param = self.check_minutes(args)
+        if mins_param['status_code'] != 200:
+            return mins_param['msg'], mins_param['status_code']
+        else:
+            mins = mins_param['minutes']
+        user_param = self.check_user(args)
+        if user_param['status_code'] != 200:
+            return user_param['msg'], user_param['status_code']
+        else:
+            userID = user_param['user']
+
+        calories = self.get_exact_calories(weight, workout, mins)
+        #db = DB()
+        insert_statement = (f"insert into Fitness (UserID, WorkoutType, Minutes, Calories) values "
+                            f"({userID}, '{workout}', {mins}, {calories});")
+        #db.insert_data(insert_statement)
+        return str(calories), 200
+
+    def get_calories_list_per_hour(self, weight):
+        calories_for_exercise = []
+        if weight < 113:
+            calories_for_exercise = [480, 336, 336, 264, 192, 384, 312, 336, 504, 792, 384, 480,
+                                     576, 408, 144, 432, 216, 384, 288, 384, 264, 288, 144, 432,
+                                     432, 384, 216, 264, 168, 192, 576, 240, 216, 288, 384, 336,
+                                     240, 480, 336, 264, 216, 216, 336, 480, 192, 336, 480, 336,
+                                     408, 792, 600, 480, 384, 336, 288, 54, 240, 288, 456, 30,
+                                     384, 336, 240, 288, 192, 288, 480, 192, 336, 288, 216, 480,
+                                     288, 144, 144, 288, 240, 288, 384, 144]
+        elif weight > 112 and weight < 138:
+            calories_for_exercise = [600, 420, 420, 330, 240, 480, 390, 420, 630, 990, 480, 600,
+                                     720, 510, 180, 540, 270, 480, 360, 480, 330, 360, 180, 540,
+                                     540, 480, 270, 330, 210, 240, 720, 300, 270, 360, 480, 420,
+                                     300, 600, 420, 330, 270, 270, 420, 600, 240, 420, 600, 420,
+                                     510, 990, 750, 600, 480, 420, 360, 68, 300, 360, 570, 38,
+                                     480, 420, 300, 360, 240, 360, 600, 240, 420, 360, 270, 600,
+                                     360, 180, 180, 360, 300, 360, 480, 180]
+        elif weight > 137 and weight < 163:
+            calories_for_exercise = [720, 504, 504, 396, 288, 576, 468, 504, 756, 1188, 576, 720,
+                                     864, 612, 216, 648, 324, 576, 432, 576, 396, 432, 216, 648,
+                                     648, 576, 324, 396, 252, 288, 864, 360, 324, 432, 576, 504,
+                                     360, 720, 504, 396, 324, 324, 504, 720, 288, 504, 720, 504,
+                                     612, 1188, 900, 720, 576, 504, 432, 81, 360, 432, 684, 45,
+                                     576, 504, 360, 432, 288, 432, 720, 288, 504, 432, 324, 720,
+                                     432, 216, 216, 432, 360, 432, 576, 216]
+        elif weight > 162 and weight < 188:
+            calories_for_exercise = [840, 588, 588, 462, 336, 672, 546, 588, 882, 1386, 672, 840,
+                                     1008, 714, 252, 756, 378, 672, 504, 672, 462, 504, 252, 756,
+                                     756, 672, 378, 462, 294, 336, 1008, 420, 378, 504, 672, 588,
+                                     420, 840, 588, 462, 378, 378, 88, 840, 336, 588, 840, 588,
+                                     714, 1386, 1050, 840, 672, 588, 504, 95, 420, 504, 798, 53,
+                                     672, 588, 420, 504, 336, 504, 840, 336, 588, 504, 378, 840,
+                                     504, 252, 252, 504, 420, 504, 672, 252]
+        else:
+            calories_for_exercise = [960, 672, 672, 528, 384, 768, 624, 672, 1008, 1584, 768, 960,
+                                     1152, 816, 288, 864, 432, 768, 576, 768, 528, 576, 288, 864,
+                                     864, 768, 432, 528, 336, 384, 1152, 480, 432, 576, 768, 672,
+                                     480, 960, 672, 528, 432, 432, 672, 960, 384, 672, 960, 672,
+                                     816, 1584, 1200, 960, 768, 672, 576, 108, 480, 576, 912, 60,
+                                     768, 672, 480, 576, 384, 576, 960, 384, 672, 576, 432, 960,
+                                     576, 288, 288, 576, 480, 576, 768, 288]
+        return calories_for_exercise
+
+    def get_exact_calories(self, weight, workout, minutes):
+        calories_for_exercise = self.get_calories_list_per_hour(weight)
+        index = self.workout_list.index(workout)
+        calories_per_min = calories_for_exercise[index] / 60
+        return (minutes*calories_per_min)
+
+    def check_weight(self, data):
+        if 'weight' in data:
+            weight = data['weight']
+            try:
+                weight = int(weight)
+                return {"status_code": 200, "weight": weight}
+            except ValueError:
+                return {"msg": "Please enter an integer for weight",
+                        "status_code": 400}
+        else:
+            return {"msg": "Please enter a weight",
+                    "status_code": 400}
+
+    def check_workout_type(self, data):
+        if 'workout' in data:
+            workout = data['workout']
+            if workout in self.workout_list:
+                return {"status_code": 200, "workout": workout}
+            else:
+                return {"msg": "Please enter a valid workout",
+                        "status_code": 400}
+        else:
+            return {"msg": "Please enter a workout",
+                    "status_code": 400}
+
+    def check_minutes(self, data):
+        if 'minutes' in data:
+            mins = data['minutes']
+            try:
+                mins = int(mins)
+                return {"status_code": 200, "minutes": mins}
+            except ValueError:
+                return {"msg": "Please enter an integer for minutes of the workout duration.",
+                        "status_code": 400}
+        else:
+            return {"msg": "Please enter a workout duration in minutes",
+                    "status_code": 400}
+
+    def check_user(self, data):
+        if 'user' in data:
+            user = data['user']
+            try:
+                user = int(user)
+                return {"status_code": 200, "user": user}
+            except ValueError:
+                return {"msg": "Please enter an integer for user ID",
+                        "status_code": 400}
+        else:
+            return {"msg": "Please enter a user",
+                    "status_code": 400}
+
