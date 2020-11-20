69b4e051eb5fe819660f2cfd283d14c2fee61414
Aditya Golatkar <aditya@aditya.lan>
2020-11-20 01:10:30 -0800

    database, tables creation

--- backend/edamam_api.py
+++ backend/edamam_api.py
@@ -11,10 +11,18 @@ class EdamamAPI():
         self.headers = {'x-rapidapi-host': self._host,
                         'x-rapidapi-key': self._key}
 
-    def get_similar(a, b):
+        self.transform_dict =  {
+                                'ENERC_KCAL': 'Cals',
+                                'PROCNT': 'Protein',
+                                'FAT': 'Fat',
+                                'CHOCDF': 'Carbs',
+                                'FIBTG': 'Fiber',
+                            }
+        
+    def get_similar(self, a, b):
         return SequenceMatcher(None, a, b).ratio()
 
-    def get_food_information(query, upc=False):
+    def get_food_information(self, query, upc=False):
 
         # Check UPC code or Ingredient
         # UPC are barcodes and Ingredients are all other foods.
@@ -28,58 +36,6 @@ class EdamamAPI():
         response = requests.request("GET", self._url, headers=self.headers, params=querystring)
 
         return response.json()
-
-    # Given the query and the upc, this function will return the nutrient information in a 
-    # dictionary, along with food label from the api and the success i.e. if it was able to 
-    # find the food item in the api.
-    def get_nutrient_information(query, upc=False):
-        food_json = self.get_food_information(query, upc)
-    
-        # food_label: Returns the food label from the API. 
-        #             Useful when we want to show the top
-        #             2 food items for that nutrient.
-    
-        # nutrient_dict: Returns the nutrient dictionary.
-        #                Keys: ENERC_KCAL(calories in kcal)
-        #                      PROCNT (protiens in g)
-        #                      FAT (fats in g)
-        #                      CHOCDF (carbohydrates in g)
-        #                      FIBTF (fibres in g)
-    
-        # success: returns when we are able to find the 
-        #          product in the API or else ask the
-        #          user to enter manually.
-    
-        food_label = None
-        food_dict = {}
-        nutrients_dict = None
-        success = False
-    
-        curr_similarity = 0
-        most_similar_id = 0
-    
-        # Checks if the food item is actually present in the API
-        # otherwise let success be false.
-        if "error" not in food_json.keys() and len(food_json['hints']) > 0:
-            # Hope that the first entry in the json file
-            # is the top match from the API.
-        
-            # Loops over all the searches and the find the one which
-            # has the closest label, using the metric define above.
-            for i,f in enumerate(food_json['hints']):
-                similarity = self.get_similar(query, f['food']['label'])
-                if similarity > curr_similarity:
-                    most_similar_id = i
-                    curr_similarity = similarity
-                
-            top_matched_item_info = food_json['hints'][most_similar_id]['food']
-            food_label = top_matched_item_info['label']
-            nutrients_dict = top_matched_item_info['nutrients']
-            success = True
-            food_dict = {'food_label': food_label}
-            food_dict.update(nutrients_dict)
-    
-        return food_dict, success
 
     def get_top_matches(self, query, upc=False, k=5):
         food_json = self.get_food_information(query, upc)
@@ -112,7 +68,7 @@ class EdamamAPI():
                     food_options_dict[i]['Label'] = matched_item_info['label']
                 food_options_dict[i]['Nutrients'] = {}
                 for k in matched_item_info['nutrients'].keys():
-                    food_options_dict[i]['Nutrients'][transform_dict[k]] = matched_item_info['nutrients'][k]
+                    food_options_dict[i]['Nutrients'][self.transform_dict[k]] = matched_item_info['nutrients'][k]
                 
             success = True
     
