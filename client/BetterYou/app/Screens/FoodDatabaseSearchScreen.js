3cfdfd95336aced92a28c01e3e84a07e28ff4448
David Lee <davkmlee@gmail.com>
2020-11-30 00:31:52 -0800

    Add validation to database search input

--- client/BetterYou/app/Screens/FoodDatabaseSearchScreen.js
+++ client/BetterYou/app/Screens/FoodDatabaseSearchScreen.js
@@ -24,24 +24,26 @@ function FoodDatabaseSearchScreen(props) {
         <TextButton
           name="Search"
           onPress={() => {
-            console.log(
-              `call search api with search query: ${query}, then set results`
-            );
-            setResults([
-              { name: "item1", subtitle: "subtitle1" },
-              { name: "item2", subtitle: "subtitle2" },
-              { name: "item3", subtitle: "subtitle3" },
-              { name: "item4", subtitle: "subtitle4" },
-              { name: "item5", subtitle: "subtitle5" },
-              { name: "item6", subtitle: "subtitle6" },
-              { name: "item7", subtitle: "subtitle7" },
-              { name: "item8", subtitle: "subtitle8" },
-              { name: "item9", subtitle: "subtitle9" },
-              { name: "item10", subtitle: "subtitle10" },
-              { name: "item11", subtitle: "subtitle11" },
-              { name: "item12", subtitle: "subtitle12" },
-              { name: "item13", subtitle: "subtitle13" },
-            ]);
+            if (query) {
+              console.log(
+                `call search api with search query: ${query}, then set results`
+              );
+              setResults([
+                { name: "item1", subtitle: "subtitle1" },
+                { name: "item2", subtitle: "subtitle2" },
+                { name: "item3", subtitle: "subtitle3" },
+                { name: "item4", subtitle: "subtitle4" },
+                { name: "item5", subtitle: "subtitle5" },
+                { name: "item6", subtitle: "subtitle6" },
+                { name: "item7", subtitle: "subtitle7" },
+                { name: "item8", subtitle: "subtitle8" },
+                { name: "item9", subtitle: "subtitle9" },
+                { name: "item10", subtitle: "subtitle10" },
+                { name: "item11", subtitle: "subtitle11" },
+                { name: "item12", subtitle: "subtitle12" },
+                { name: "item13", subtitle: "subtitle13" },
+              ]);
+            }
           }}
           style={styles.button}
         />
