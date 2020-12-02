097b6233046ae1dbfe5bf214ce831fbdbc1db5ba
AdityaGolatkar <adityagolatkar09@gmail.com>
2020-12-01 17:48:59 -0800

    merged tests

--- backend/backend_tests/run_all_tests.sh
+++ backend/backend_tests/run_all_tests.sh
@@ -3,10 +3,10 @@
 python3.7 set_up_test_db.py
 python3.7 test_db.py
 python3.7 test_staywell.py
-python3.7 test_diet_aditya.py
+python3.7 test_diet.py
 python3.7 test_edamam_api.py
-python3.7 test_fitness_aditya.py
+python3.7 test_fitness.py
 python3.7 test_goals.py
-python3.7 test_sleep_aditya.py
+python3.7 test_sleep.py
 python3.7 test_user.py
 python3.7 drop_test_tables.py
