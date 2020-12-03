2fe8893b7039270f74bab4c835db7edff0c74df9
Naveena Attota <naveena.attota@gmail.com>
2020-12-02 17:26:33 -0800

    fix for coverage report

--- backend/backend_tests/run_all_tests.sh
+++ backend/backend_tests/run_all_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /usr/local/bin/python3 set_up_test_db.py
-coverage run -m unittest discover tests 
+coverage run --source=../../backend -m unittest discover tests
 coverage report -m
 /usr/local/bin/python3 drop_test_tables.py
